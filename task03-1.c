c8488fa1816efd2ce0d7e0748ff19eaafbf95202
Mira <k.sivka@yandex.ru>
2022-02-07 13:34:26 +0300

    LAB 03 done

---
+++ task03-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+
+    printf("\narguments:\n\n");
+    for (int i = 0; i < argc; ++i) {
+        printf("aerv[%d]\t=\t%s\n", i, argv[i]);
+    }
+    printf("\n----------------------------\n");
+    printf("\nenvironment params:\n\n");
+    int i = 0;
+    while (envp[i] != NULL) {
+        printf("envp[%d]\t=\t%s\n", i, envp[i]);
+        i++;
+    }
+    return 0;
+}
